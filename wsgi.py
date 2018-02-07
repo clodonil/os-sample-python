eeebbc9d2f7708fa72a71f5019cb51ccf453088b
Clodonil Honorio Trigo <clodonil@nisled.org>
2018-02-07 17:05:48 -0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1><center>OpenShift Deploy Python - Flash!</center?</h1>"
+    return "<h1><center>OpenShift Deploy Python - Flash!</center></h1>"
 
 if __name__ == "__main__":
     application.run()
